9a69feb872b18179a7214729b742fba93ae90960
Michael Nash <michael@spatialnetworks.com>
2018-04-05 17:14:05 -0400

    got the linter working

---
+++ blog_app/spec/features/editing_article_spec.rb
@@ -0,0 +1,21 @@
+equire "rails_helper"
+
+RSpec.feature "Editing Articles" do
+  before do
+    @article1 = Article.create(title: "The first article", body: "Adipisicing excepteur ullamco aliqua ut cupidatat anim do quis ullamco nisi.")
+  end
+
+  scenario "A user updates an article" do
+    visit "/"
+
+    click_link @article.title
+    click_link "Edit Article"
+
+    fill_in "Title",	with: "Ex adipisicing eu do tempor tempor culpa laboris irure amet mollit culpa velit."
+    fill_in "Body",	with: "Excepteur voluptate tempor voluptate cillum occaecat commodo velit."
+    click_button "Update Article"
+
+    expect(page).to have_content("Article has been updated")
+    expect(page.current_path).to eq(article_path(@article))
+  end
+end
