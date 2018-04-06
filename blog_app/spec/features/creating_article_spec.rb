56c29e8d0666e748091ccb59a37d26d8460d372e
Michael Nash <michael@spatialnetworks.com>
2018-04-06 14:17:59 -0400

    form refactoring

--- blog_app/spec/features/creating_article_spec.rb
+++ blog_app/spec/features/creating_article_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Creating Articles" do
     fill_in "Title",	with: "Creating a blog"
     fill_in "Body",	with: "Lorem Ipsum"
 
-    click_button "Create Article"
+    click_button "Update Article"
 
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
@@ -23,7 +23,7 @@ RSpec.feature "Creating Articles" do
     fill_in "Title",	with: ""
     fill_in "Body",	with: ""
 
-    click_button "Create Article"
+    click_button "Update Article"
 
     expect(page).to have_content("Article has not been created")
     expect(page).to have_content("Title can't be blank")
