723e1b0a19ef213115df73e598cbb583afa4ea5c
Michael Nash <michael@spatialnetworks.com>
2018-04-11 12:23:20 -0400

    restricted new article button to users who are signed in

--- blog_app/spec/features/listing_article_spec.rb
+++ blog_app/spec/features/listing_article_spec.rb
@@ -3,13 +3,25 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
   before do
     @john = User.create(email: "john@example.com", password: "password")
-    login_as(@john)
     @article1 = Article.create(title: "The first article", body: "Adipisicing excepteur ullamco aliqua ut cupidatat anim do quis ullamco nisi.", user: @john)
     @article2 = Article.create(title: "The second article", body: "Id pariatur Lorem mollit nostrud non occaecat enim magna excepteur duis pariatur laboris.", user: @john)
   end
 
-  scenario "A user lists all articles" do
+  scenario "with articles created and user not signed in" do
     visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created and user signed in" do
+    visit "/"
+    login_as(@john)
 
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
