24eeddb95e872bde32e8834af96743a26ae07271
Michael Nash <michael@spatialnetworks.com>
2018-04-11 14:58:03 -0400

    added user authentication for edit and delete actions

--- blog_app/app/controllers/articles_controller.rb
+++ blog_app/app/controllers/articles_controller.rb
@@ -25,9 +25,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    unless user_signed_in?
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to root_path
+    end
+    unless @article.user == current_user
+      flash[:alert] = "You can only edit your own article."
+      redirect_to root_path
+    end
   end
 
   def update
+    unless user_signed_in?
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to root_path
+    end
+    unless @article.user == current_user
+      flash[:alert] = "You can only edit your own article."
+      redirect_to root_path
+    end
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
@@ -38,6 +54,14 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    unless user_signed_in?
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to root_path
+    end
+    unless @article.user == current_user
+      flash[:alert] = "You can only delete your own article."
+      redirect_to root_path
+    end
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
--- blog_app/spec/requests/articles_spec.rb
+++ blog_app/spec/requests/articles_spec.rb
@@ -3,7 +3,81 @@ require "rails_helper"
 RSpec.describe "Articles", type: :request do
 
   before do
-    @article = Article.create(title: "Nulla reprehenderit quis mollit nulla ullamco anim voluptate elit proident cillum amet ex duis ea.", body: "Veniam in laborum voluptate reprehenderit veniam in minim officia aliquip consectetur nulla ea ut.")
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "john@example.com", password: "password")
+    @article = Article.create!(title: "Nulla reprehenderit quis mollit nulla ullamco anim voluptate elit proident cillum amet ex duis ea.", body: "Veniam in laborum voluptate reprehenderit veniam in minim officia aliquip consectetur nulla ea ut.", user: @john)
+  end
+
+  describe 'GET /articles/:id/edit' do
+    context 'with non-signed-in user' do
+      before { get "/articles/#{@article.id}/edit"}
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with signed-in user who is non-owner' do
+      before do
+        login_as(@fred)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with signed-in user who is owner successful edit' do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "successfully edits article" do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'DELETE /articles/edit' do
+    context 'with non-signed-in user' do
+      before { delete "/articles/#{@article.id}"}
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with signed-in user who is non-owner' do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with signed-in user who is owner successful delete' do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "successfully deletes article" do
+        expect(response.status).to eq 200
+      end
+    end
   end
 
   describe 'GET /articles/:id' do
